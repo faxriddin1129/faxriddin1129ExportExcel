ae480d1b60a69b49cecb009793ad39de3838e8ed
Faxriddin Boboyev <79299905+faxriddin1129@users.noreply.github.com>
2022-08-04 17:38:54 +0500

    Update
    Update

--- main.js
+++
@@ -1,8 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/table2excel@1.0.4/dist/table2excel.min.js"></script>
-<script>
-    document.getElementById("btn").addEventListener("click", () => {
-        let table2excel = new Table2Excel();
-        table2excel.export(document.querySelector("#Record"));
-    });
-
-</script>
